3602b010f9fd37f941d208a643d22128d05f7ea1
ajraduenz <aldoraduenzjunior@gmail.com>
2022-08-29 20:57:55 -0300

    Add loop sass

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ function App() {
   return (
     <div className="App">
       
-      <p>Lorem Ipsum</p>
+      <p>Lorem Ipsum <span>delta beta</span></p>
+      <div className='grid'>
+        <div className='box opacity-1'>a</div>
+        <div className='box opacity-2'>a</div>
+        <div className='box opacity-3'>a</div>
+        <div className='box opacity-4'>a</div>
+        <div className='box opacity-5'>a</div>
+        <div className='box opacity-6'>a</div>
+        <div className='box opacity-7'>a</div>
+        <div className='box opacity-8'>a</div>
+        <div className='box opacity-9'>a</div>
+        <div className='box opacity-10'>a</div>
+      </div>
     </div>
   );
 }
