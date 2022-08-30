59b7c7551b6eb2dbe24de41617fa3a6110afd264
ajraduenz <aldoraduenzjunior@gmail.com>
2022-08-29 22:41:39 -0300

    Redux with switch

--- src/index.js
+++ src/index.js
@@ -1,31 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reportWebVitals from './reportWebVitals';
-import { configureStore } from '@reduxjs/toolkit';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
+import reportWebVitals from "./reportWebVitals";
+// redux
+import store from './store/configureStore'
+import { Provider } from "react-redux";
 
-function reducer(state = 0, action){
-  switch (action.type){
-    case 'INCREMENTAR':
-      return state + 1;
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION && window.__REDUX_DEVTOOLS_EXTENSION())
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/store/contador.js
@@ -0,0 +1,11 @@
+
+function contador(state = 0, action) {
+  switch (action.type) {
+    case "INCREMENTAR":
+      return state + 1;
+    default:
+      return state;
+  }
+}
+
+export default contador
