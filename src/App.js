06f98384056f4545b27b7488c146c944a54fea56
ajraduenz <aldoraduenzjunior@gmail.com>
2022-08-29 22:31:01 -0300

    redux without toolkit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.scss';
+import logo from "./logo.svg";
+import "./App.scss";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      
-      <p>Lorem Ipsum <span>delta beta</span></p>
-      <div className='grid'>
-        <div className='box opacity-1'>a</div>
-        <div className='box opacity-2'>a</div>
-        <div className='box opacity-3'>a</div>
-        <div className='box opacity-4'>a</div>
-        <div className='box opacity-5'>a</div>
-        <div className='box opacity-6'>a</div>
-        <div className='box opacity-7'>a</div>
-        <div className='box opacity-8'>a</div>
-        <div className='box opacity-9'>a</div>
-        <div className='box opacity-10'>a</div>
+      <p>
+        Lorem Ipsum <span>delta beta</span>
+      </p>
+      <div className="grid">
+        <div className="box opacity-1">a</div>
+        <div className="box opacity-2">a</div>
+        <div className="box opacity-3">a</div>
+        <div className="box opacity-4">a</div>
+        <div className="box opacity-5">a</div>
+        <div className="box opacity-6">a</div>
+        <div className="box opacity-7">a</div>
+        <div className="box opacity-8">a</div>
+        <div className="box opacity-9">a</div>
+        <div className="box opacity-10">a</div>
+      </div>
+      <div>
+        <p>Total:{state}</p>
+        <button onClick={() => dispatch({ type: "INCREMENTAR" })}>Somar</button>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+
+
+function reducer(state = 0, action){
+  switch (action.type){
+    case 'INCREMENTAR':
+      return state + 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION && window.__REDUX_DEVTOOLS_EXTENSION())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
